5b91fb733b8c1e90819b4ad4e00f327528060374
JefkeB <github@bergenv.nl>
2020-08-21 14:14:15 +0200

    defined all the available pins

--- src/xpro/variant/atmel_samd21_xpro_v1/pins.h
+++ src/xpro/variant/atmel_samd21_xpro_v1/pins.h
@@ -1,5 +1,69 @@
 #ifndef PINS_H_INCLUDED
 #define PINS_H_INCLUDED
 
+#define PIN_PB09        (0ul)
+#define PIN_PB08        (1ul)
+
+#define PIN_PB30        (2ul)
+#define PIN_PA15        (3ul)
+
+#define PIN_PB06        (4ul)
+#define PIN_PB07        (5ul)
+#define PIN_PB04        (6ul)
+#define PIN_PB05        (7ul)
+#define PIN_PA20        (8ul)
+#define PIN_PA21        (9ul)
+#define PIN_PB14        (10ul)
+#define PIN_PB15        (11ul)
+#define PIN_PB30        (12ul)
+#define PIN_PA15        (13ul)
+#define PIN_PA28        (14ul)
+#define PIN_PA27        (15ul)
+
+#define PIN_PB02        (16l)
+#define PIN_PB03        (17ul)
+#define PIN_PB12        (18ul)
+#define PIN_PB13        (19ul)
+#define PIN_PA12        (20ul)
+#define PIN_PA13        (21ul)
+
+#define PIN_PB00        (22ul)
+#define PIN_PB01        (23ul)
+#define PIN_PA10        (24ul)
+#define PIN_PA11        (25ul)
+#define PIN_PA02        (26ul)
+#define PIN_PA03        (27ul)
+
+#define PIN_PA23        (28ul)
+#define PIN_PA22        (29ul)
+
+#define PIN_PB11        (30ul)
+#define PIN_PB10        (31ul)
+
+#define PIN_PA09        (32ul)
+#define PIN_PA08        (33ul)
+
+#define PIN_PA06        (34ul)
+#define PIN_PA04        (35ul)
+#define PIN_PA07        (36ul)
+#define PIN_PA05        (37ul)
+#define PIN_PB05        (38ul)
+
+#define PIN_PA18        (39ul)
+#define PIN_PA16        (40ul)
+#define PIN_PA19        (41ul)
+#define PIN_PA17        (42ul)
+#define PIN_PB15        (43ul)
+
+#define PIN_PB22        (44ul)
+#define PIN_PB16        (45ul)
+#define PIN_PB23        (46ul)
+#define PIN_PB17        (47ul)
+#define PIN_PA27        (48ul)
+
+#define PIN_PA25        (49ul)
+#define PIN_PA24        (50ul)
+#define PIN_PA14        (51ul)
+#define PIN_PA03        (52ul)
 
 #endif
